a11ff2741da19257037482f4732950b17f08fa97
kmarcel1 <kmwconsultants@gmail.com>
2016-04-08 21:41:24 -0500

    My first Project
    The coding is in VBasic using Visual Studios.

---
+++ First Project ReadMe.txt
@@ -0,0 +1,10 @@
+ReadMe File
+
+Project Details
+
+Data Communications Corp wants a small program that will calculate the cost of
+UTP it installs for their clients. Write a program that will ask the user to 
+input the name of the client, the number of feet of cable installed. The program
+should then calculate and display a final bill. Cost per foot of UTP is .21 cents.
+Be sure to add a tax of 6%. Final bill should include the total cost and client
+name. Be sure to add modules to your program.
---
+++ Lab 4.3/Lab 4.3/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        'declare variables
+        Dim clientName As String = ""
+        Dim feetUTP As Double = 0
+        Dim subTotal As Double = 0
+        Dim taxCost As Double = 0
+        Dim totalCost As Double = 0
+
+        'get input
+        inputData(clientName, feetUTP)
+
+        'calculations
+        calcCosts(subTotal, taxCost, totalCost, feetUTP)
+
+        'Output
+        displayBill(clientName, totalCost)
+
+        'hard pause
+        Console.WriteLine(" Press ENTER to continue...")
+        Console.ReadLine()
+    End Sub
+
+    Sub inputData(ByRef clientName As String, ByRef feetUTP As Double)
+        Console.Write("What is the client name: ")
+        clientName = Console.ReadLine()
+        Console.Write("How many feet of cable was installed? ")
+        feetUTP = Console.ReadLine()
+    End Sub
+
+    Sub calcCosts(ByRef subTotal As Double, ByRef taxCost As Double, ByRef totalCost As Double, ByVal feetUTP As Double)
+        subTotal = feetUTP * 0.21
+        taxCost = subTotal * 0.06
+        totalCost = subTotal + taxCost
+    End Sub
+
+    Sub displayBill(ByVal clientName As String, ByVal totalCost As Double)
+        Console.WriteLine("The client name is " & clientName)
+        Console.WriteLine("The total cost will be $" & totalCost)
+    End Sub
+
+End Module
